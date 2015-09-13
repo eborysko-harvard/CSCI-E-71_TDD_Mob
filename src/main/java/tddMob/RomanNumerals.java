742bd17d2f67710b72f45579d3ec17058358d2d2
Justin Sanford <justin.h.sanford@gmail.com>
2015-09-13 10:31:58 -0700

    initial

---
+++ src/main/java/tddMob/RomanNumerals.java
@@ -0,0 +1,42 @@
+package tddMob;
+
+public class RomanNumerals {
+	public static int toInt(String s) {
+		int sum = 0;
+		char[] ar = s.toCharArray();
+		
+		for (int i = 0; i < ar.length-1; i++) {
+			if (lookup(ar[i]) < lookup(ar[i+1]) )
+			{
+				sum -= lookup(ar[i]);
+			}
+			else
+			{
+				sum += lookup(ar[i]);
+			}
+		}
+		sum += lookup(ar[ar.length-1]);
+		return sum;
+		
+	}
+	private static int lookup(char c) {
+		switch(c) {
+			case 'I':
+				return 1;
+			case 'V':
+				return 5;
+			case 'X':
+				return 10;
+			case 'L' : 
+				return 50;
+			case 'C' :
+				return 100;
+			case 'D' :
+				return 500;
+			case 'M':
+				return 1000;
+			default:
+				return 0;
+		}
+	}
+}
---
+++ src/test/java/tddMob/RomanNumeralsTest.java
@@ -0,0 +1,51 @@
+package tddMob;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class RomanNumeralsTest {
+
+	@Test
+	public void testBaseRomanNumerals() {
+		assertEquals(1, RomanNumerals.toInt("I"));
+		assertEquals(5, RomanNumerals.toInt("V"));
+		assertEquals(10, RomanNumerals.toInt("X"));
+		assertEquals(50, RomanNumerals.toInt("L"));
+		assertEquals(100, RomanNumerals.toInt("C"));
+		assertEquals(500, RomanNumerals.toInt("D"));
+		assertEquals(1000, RomanNumerals.toInt("M"));
+	}
+	
+	@Test
+	public void testAddRomanNumerals() {
+		assertEquals(2, RomanNumerals.toInt("II"));
+		assertEquals(3, RomanNumerals.toInt("III"));
+		assertEquals(6, RomanNumerals.toInt("VI"));
+		assertEquals(15, RomanNumerals.toInt("XV"));
+		
+		assertEquals(3888, RomanNumerals.toInt("MMMDCCCLXXXVIII"));
+		
+	}
+		
+	@Test
+	public void testSubrtractRomanNumerals() {
+		assertEquals(4, RomanNumerals.toInt("IV"));
+		assertEquals(9, RomanNumerals.toInt("IX"));
+		assertEquals(40, RomanNumerals.toInt("XL"));
+		assertEquals(90, RomanNumerals.toInt("XC"));
+		assertEquals(900, RomanNumerals.toInt("CM"));
+		
+	}
+		
+	@Test
+	public void testAddNSubtract() {
+		assertEquals(41, RomanNumerals.toInt("XLI"));
+		assertEquals(44, RomanNumerals.toInt("XLIV"));
+		assertEquals(95, RomanNumerals.toInt("XCV"));
+		assertEquals(48, RomanNumerals.toInt("XLVIII"));
+		assertEquals(49, RomanNumerals.toInt("XLIX"));
+		assertEquals(2991, RomanNumerals.toInt("MMXMI"));
+		
+	}
+}
