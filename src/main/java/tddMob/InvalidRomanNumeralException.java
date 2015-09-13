d9dc9b16a08f6c6575bf3907d8a94bbab19ccdc1
Justin Sanford <justin.h.sanford@gmail.com>
2015-09-13 11:34:29 -0700

    completed

---
+++ src/main/java/tddMob/InvalidRomanNumeralException.java
@@ -0,0 +1,10 @@
+package tddMob;
+
+public class InvalidRomanNumeralException extends RuntimeException {
+
+	public InvalidRomanNumeralException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+}
